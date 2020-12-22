eaae79c59daf0022b7fd56758bc9a89822575c08
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-22 22:17:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,14 +1,14 @@
 require 'pry'
 def starts_with_a_vowel?(word)
 if word.match(/\A[aeiou]/i) == nil
-  return true
-else 
   return false
+else 
+  return true
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+word.start(/\)
 end
 
 def words_five_letters_long(text)
