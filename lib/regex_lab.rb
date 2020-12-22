d11d78bb79274630605a8f982a83b220a213355d
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-22 22:21:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$/) == nil
+if text.match(/^[A-Z].*\.$/) == nil
     return false
 else
     return true
@@ -25,5 +25,9 @@ end
 
 
 def valid_phone_number?(phone)
-if phone.match
+if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+else
+    return true
+  end
 end
