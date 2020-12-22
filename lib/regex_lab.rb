2ad0038b078d83598afceae2d53d50deb2873571
Austin Hernandez <ahernandez1170@gmail.com>
2020-12-22 22:15:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,10 @@
-require pry
+require 'pry'
 def starts_with_a_vowel?(word)
-
+if word.start(/\A[aeiou]\b/)
+  return true
+else 
+  return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
